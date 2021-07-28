bbd719397d917d1dc9f29347665dd28e01c20475
Lei, Lei <lei.lei@emc.com>
2021-07-28 21:46:32 +0800

    Update layer_print_tree.cpp

--- 02_Tree/layer_print_tree.cpp
+++ 02_Tree/layer_print_tree.cpp
@@ -7,22 +7,26 @@ struct Node {
     int data;
     Node * left;
     Node * right;
-    Node(int d) : data(d) {left = right = NULL;}
+    Node(int d, Node *l=nullptr, Node *r=nullptr) : data(d), left(l), right(r) {}
     
 };
 
 void layer_print(Node * head)
 {
-    if (head == NULL) return; 
+    if (head == nullptr) return;
     
     queue<Node *> node_queue;
     node_queue.push(head);
+    
     while (!node_queue.empty()) {
         Node * tmp = node_queue.front();
-        cout << tmp->data << ", "; 
-        if (tmp->left != NULL) node_queue.push(tmp->left);
-        if (tmp->right != NULL) node_queue.push(tmp->right);
         node_queue.pop();
+        
+        if (tmp != nullptr) {
+            cout << tmp->data << " "; 
+            if (tmp->left != nullptr)  node_queue.push(tmp->left);
+            if (tmp->right != nullptr) node_queue.push(tmp->right);
+        }
     }
     
 }
@@ -30,11 +34,11 @@ void layer_print(Node * head)
 int main()
 {
     Node a(10), b(20), c(30), d(40), e(50), f(60);
-    a.left = &b;
+    a.left  = &b;
     a.right = &c;
-    b.left = &d;
+    b.left  = &d;
     b.right = &e;
-    c.left = &f; 
+    c.left  = &f; 
     
     layer_print(&a);
     
